91d8ff0d47f046ded683fe91ac9368eccfd26f5b
Claudio Raimondi <claudio.raimondi@protonmail.com>
2025-03-15 12:48:08 +0100

    Fix overwrite issues

--- Logger.cpp
+++ Logger.cpp
@@ -1,3 +1,14 @@
+/*================================================================================
+
+File: Logger.cpp                                                                
+Creator: Claudio Raimondi                                                       
+Email: claudio.raimondi@pm.me                                                   
+
+created at: 2025-03-15 12:48:08                                                 
+last edited: 2025-03-15 12:48:08                                                
+
+================================================================================*/
+
 #include "Logger.hpp"
 #include "utils.hpp"
 
@@ -11,18 +22,23 @@
 Logger::Logger(const std::string_view filename) :
   filename(filename),
   fd(create_file()),
-  buffer(static_cast<char *>(aligned_alloc(ALIGNMENT, BUFFER_SIZE))),
-  write_ptr(buffer),
-  end_ptr(buffer + BUFFER_SIZE),
-  iov(precompute_iov())
+  buffers({
+    static_cast<char *>(aligned_alloc(ALIGNMENT, BUFFER_SIZE)),
+    static_cast<char *>(aligned_alloc(ALIGNMENT, BUFFER_SIZE))
+  }),
+  buf_idx(0),
+  write_ptr(buffers[buf_idx]),
+  end_ptr(buffers[buf_idx] + BUFFER_SIZE)
 {
   bool error = false;
 
   error |= (fallocate(fd, FALLOC_FL_KEEP_SIZE, 0, PREALLOCATED_FILE_SIZE) == -1);
-  error |= (io_uring_queue_init(QUEUE_DEPTH, &ring, 0) == -1);
+  error |= (io_uring_queue_init(1, &ring, 0) == -1);
   error |= (io_uring_register_files(&ring, &fd, 1) == -1);
-  error |= (io_uring_register_buffers(&ring, iov.data(), N_CHUNKS) == -1);
-  error |= (madvise(buffer, BUFFER_SIZE, MADV_SEQUENTIAL) == -1);
+  iovec iov[2] = {{buffers[0], BUFFER_SIZE}, {buffers[1], BUFFER_SIZE}};
+  error |= (io_uring_register_buffers(&ring, iov, 2) == -1);
+  error |= (madvise(buffers[0], BUFFER_SIZE, MADV_SEQUENTIAL) == -1);
+  error |= (madvise(buffers[1], BUFFER_SIZE, MADV_SEQUENTIAL) == -1);
   error |= (posix_fadvise(fd, 0, 0, POSIX_FADV_SEQUENTIAL) == -1);
 
   if (error)
@@ -39,25 +55,12 @@ int Logger::create_file(void) const
   return fd;
 }
 
-const std::array<iovec, N_CHUNKS> Logger::precompute_iov(void)
-{
-  constexpr size_t chunk_size = BUFFER_SIZE / N_CHUNKS;
-  std::array<iovec, N_CHUNKS> result = {};
-  
-  for (uint16_t i = 0; i < N_CHUNKS; ++i)
-  {
-    result[i].iov_base = buffer + (i * chunk_size);
-    result[i].iov_len = chunk_size;
-  }
-
-  return result;
-}
-
 Logger::~Logger()
 {
-  free(buffer);
   io_uring_unregister_files(&ring);
   io_uring_queue_exit(&ring);
+  free(buffers[0]);
+  free(buffers[1]);
   close(fd);
 }
 
@@ -68,13 +71,13 @@ void Logger::log(const std::string_view message)
 
   while (remaining > 0)
   {
-    size_t to_copy = utils::min<size_t>(remaining, end_ptr - write_ptr);
+    const size_t to_copy = utils::min<size_t>(remaining, end_ptr - write_ptr);
     std::memcpy(write_ptr, data, to_copy);
     write_ptr += to_copy;
     data += to_copy;
     remaining -= to_copy;
 
-    if (write_ptr == end_ptr)
+    if (UNLIKELY(write_ptr == end_ptr))
       flush();
   }
 }
@@ -82,10 +85,13 @@ void Logger::log(const std::string_view message)
 void Logger::flush(void)
 {
   io_uring_sqe *sqe = io_uring_get_sqe(&ring);
+  io_uring_prep_write_fixed(sqe, fd, buffers[buf_idx], BUFFER_SIZE, -1, buf_idx);
+  sqe->flags |= IOSQE_ASYNC | IOSQE_FIXED_FILE | IOSQE_IO_LINK | IOSQE_BUFFER_SELECT | IOSQE_CQE_SKIP_SUCCESS;
 
-  io_uring_prep_writev(sqe, fd, iov.data(), N_CHUNKS, -1);
-  sqe->flags |= IOSQE_ASYNC | IOSQE_FIXED_FILE | IOSQE_IO_LINK | IOSQE_CQE_SKIP_SUCCESS;
-  if (io_uring_submit(&ring) < 0)
+  if (UNLIKELY(io_uring_submit(&ring) < 0))
     utils::throw_error("Failed to submit writev operation");
-  write_ptr = buffer;
+
+  buf_idx ^= 1;
+  write_ptr = buffers[buf_idx];
+  end_ptr = write_ptr + BUFFER_SIZE;
 }
--- Logger.hpp
+++ Logger.hpp
@@ -8,10 +8,8 @@
 #include <liburing.h>
 #include <memory>
 
-constexpr size_t BUFFER_SIZE = 64 * 1024 * 1024;
-constexpr uint8_t N_CHUNKS = 64;
+constexpr size_t BUFFER_SIZE = 32 * 1024 * 1024;
 constexpr uint16_t ALIGNMENT = 4096;
-constexpr uint8_t QUEUE_DEPTH = 16;
 constexpr size_t PREALLOCATED_FILE_SIZE = 365ULL * 24 * 60 * 60 * 100;
 
 class Logger
@@ -26,14 +24,13 @@ class Logger
   private:
 
     int create_file(void) const;
-    const std::array<iovec, N_CHUNKS> precompute_iov(void);
     void flush(void);
 
     const std::string filename;
     const int fd;
-    char *buffer;
+    char *buffers[2];
+    uint8_t buf_idx;
     char *write_ptr;
-    const char *end_ptr; 
-    const std::array<iovec, N_CHUNKS> iov;
+    const char *end_ptr;
     io_uring ring;
 };
