e96b8a7a1e16bd79069f54d309e1d6f360476408
Claudio Raimondi <claudio.raimondi@protonmail.com>
2025-03-22 22:21:15 +0100

    Add prefetch macro

--- src/macros.hpp
+++ src/macros.hpp
@@ -11,10 +11,13 @@ last edited: 2025-03-08 21:15:54
 
 #pragma once
 
-#define LIKELY(x)       __builtin_expect(!!(x), 1)
-#define UNLIKELY(x)     __builtin_expect(!!(x), 0)
-#define HOT             __attribute__((hot))
-#define COLD            __attribute__((cold))
-#define ALWAYS_INLINE   __attribute__((always_inline))
-#define NEVER_INLINE    __attribute__((noinline))
-#define UNREACHABLE     __builtin_unreachable()
+#define LIKELY(x)                 __builtin_expect(!!(x), 1)
+#define UNLIKELY(x)               __builtin_expect(!!(x), 0)
+#define HOT                       __attribute__((hot))
+#define COLD                      __attribute__((cold))
+#define ALWAYS_INLINE             __attribute__((always_inline))
+#define NEVER_INLINE              __attribute__((noinline))
+#define UNREACHABLE               __builtin_unreachable()
+#define PREFETCH_R(x, priority)   __builtin_prefetch(x, 0, priority)
+#define PREFETCH_W(x, priority)   __builtin_prefetch(x, 1, priority)
+#define PREFETCH_RW(x, priority)  __builtin_prefetch(x, 1, priority)
