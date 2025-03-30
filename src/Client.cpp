6b8f1eadee2b872b92e29cb656c0b792bb28b648
Claudio Raimondi <claudio.raimondi@protonmail.com>
2025-03-30 15:19:39 +0200

    Optimize run loop for cache locality

--- src/Client.cpp
+++ src/Client.cpp
@@ -5,7 +5,7 @@ Creator: Claudio Raimondi
 Email: claudio.raimondi@pm.me                                                   
 
 created at: 2025-03-14 19:09:39                                                 
-last edited: 2025-03-29 14:50:25                                                
+last edited: 2025-03-30 15:19:39                                                
 
 ================================================================================*/
 
@@ -19,6 +19,7 @@ last edited: 2025-03-29 14:50:25
 #include <endian.h>
 
 #include "Client.hpp"
+#include "config.hpp"
 #include "macros.hpp"
 #include "error.hpp"
 
@@ -60,8 +61,8 @@ COLD std::vector<int> Client::createSockets(const std::vector<std::string> &addr
     mreq.imr_multiaddr.s_addr = multicast_address.sin_addr.s_addr;
 
     const int sock_fd = createUdpSocket();
-    error |= (bind(sock_fd, reinterpret_cast<const sockaddr *>(&bind_address), sizeof(bind_address)) == -1);
-    error |= (setsockopt(sock_fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) == -1);
+    error |= bind(sock_fd, reinterpret_cast<const sockaddr *>(&bind_address), sizeof(bind_address)) == -1;
+    error |= setsockopt(sock_fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) == -1;
 
     sockets.push_back(sock_fd);
   }
@@ -82,7 +83,7 @@ COLD int Client::createEpollFd(void) const
     event.events = EPOLLIN | EPOLLERR | EPOLLHUP | EPOLLET;
     event.data.fd = socket;
 
-    error |= (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, socket, &event) == -1);
+    error |= epoll_ctl(epoll_fd, EPOLL_CTL_ADD, socket, &event) == -1;
   }
 
   CHECK_ERROR;
@@ -100,16 +101,17 @@ COLD int Client::createUdpSocket(void) const
   constexpr int enable = 1;
   constexpr int disable = 0;
   //constexpr int priority = 255;
+  // constexpr int recv_bufsize = SOCK_BUFSIZE;
 
-  //TODO add
-  // error |= (setsockopt(sock_fd, SOL_SOCKET, SO_PRIORITY, &priority, sizeof(enable)) == -1);
-  // error |= (setsockopt(sock_fd, SOL_SOCKET, SO_ZEROCOPY, &enable, sizeof(enable)) == -1);
   error |= (setsockopt(sock_fd, IPPROTO_IP, IP_MULTICAST_LOOP, &disable, sizeof(disable)) == -1);
   error |= (setsockopt(sock_fd, SOL_SOCKET, SO_BUSY_POLL, &enable, sizeof(enable)) == -1);
+  // error |= (setsockopt(sock_fd, SOL_SOCKET, SO_PRIORITY, &priority, sizeof(enable)) == -1);
+  // error |= (setsockopt(sock_fd, SOL_SOCKET, SO_ZEROCOPY, &enable, sizeof(enable)) == -1);
+  // error |= setsockopt(sock_fd, SOL_SOCKET, SO_RCVBUF, &recv_bufsize, sizeof(recv_bufsize)) == -1;
 
   //TODO remove
-  error |= (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) == -1);
-  error |= (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(enable)) == -1);
+  error |= setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) == -1;
+  error |= setsockopt(sock_fd, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(enable)) == -1;
 
   CHECK_ERROR;
 
@@ -118,24 +120,23 @@ COLD int Client::createUdpSocket(void) const
 
 COLD void Client::run(void)
 {
-  constexpr uint8_t MAX_PACKETS = 64;
-  constexpr uint16_t MTU = 2000;
   constexpr uint16_t MAX_MSG_SIZE = MTU - sizeof(MoldUDP64Header);
 
   //+1 added for safe prefetching past the last packet 
-  alignas(64) mmsghdr packets[MAX_PACKETS + 1]{};
-  alignas(64) iovec iov[MAX_PACKETS + 1][2]{};
-  alignas(64) MoldUDP64Header headers[MAX_PACKETS + 1]{};
-  alignas(64) char payloads[MAX_PACKETS + 1][MAX_MSG_SIZE]{};
+  alignas(64) mmsghdr mmsgs[MAX_BURST_PACKETS+1]{};
+  alignas(64) iovec iov[2][MAX_BURST_PACKETS+1]{};
+  alignas(64) struct Packet {
+    MoldUDP64Header header;
+    char payload[MAX_MSG_SIZE];
+  } packets[MAX_BURST_PACKETS+1]{};
 
-  for (uint8_t i = 0; i < MAX_PACKETS; ++i)
+  for (int i = 0; i < MAX_BURST_PACKETS; ++i)
   {
-    iov[i][0] = { &headers[i], sizeof(headers[i]) };
-    iov[i][1] = { payloads[i], sizeof(payloads[i]) };
+    iov[0][i] = { &packets[i].header, sizeof(MoldUDP64Header) };
+    iov[1][i] = { packets[i].payload, MAX_MSG_SIZE };
 
-    msghdr &msg_hdr = packets[i].msg_hdr;
-    msg_hdr.msg_iov = iov[i];
-    msg_hdr.msg_iovlen = 2;
+    mmsgs[i].msg_hdr.msg_iov = &iov[0][i];
+    mmsgs[i].msg_hdr.msg_iovlen = 2;
   }
 
   std::vector<epoll_event> events(sockets.size());
@@ -151,23 +152,22 @@ COLD void Client::run(void)
       const int fd = event.data.fd;
       error |= (event.events & (EPOLLERR | EPOLLHUP));
 
-      int8_t packets_count = recvmmsg(fd, packets, MAX_PACKETS, MSG_WAITFORONE, nullptr);
+      int8_t packets_count = recvmmsg(fd, mmsgs, MAX_BURST_PACKETS, MSG_WAITALL, nullptr);
       error |= (packets_count == -1);
-
-      const MoldUDP64Header *header_ptr = headers;
-      const char *payload_ptr = reinterpret_cast<char *>(payloads);
+      const Packet *packet = packets;
 
       while (packets_count--)
       {
-        PREFETCH_R(header_ptr + 1, 2);
-        PREFETCH_R(payload_ptr + MAX_MSG_SIZE, 2);
+        PREFETCH_R(packet + 1, 1);
 
-        const uint16_t message_count = be16toh(header_ptr->message_count);
-        processMessageBlocks(payload_ptr, message_count);
+        const MoldUDP64Header &header = packet->header;
+        const uint16_t message_count = be16toh(header.message_count);
 
-        header_ptr++;
-        payload_ptr += MAX_MSG_SIZE;
+        processMessageBlocks(packet->payload, message_count);
+
+        packet++;
       }
+      CHECK_ERROR;
     }
     CHECK_ERROR;
   }
---
+++ src/config.hpp
@@ -0,0 +1,14 @@
+/*================================================================================
+
+File: config.hpp                                                                
+Creator: Claudio Raimondi                                                       
+Email: claudio.raimondi@pm.me                                                   
+
+created at: 2025-03-30 15:01:52                                                 
+last edited: 2025-03-30 15:19:39                                                
+
+================================================================================*/
+
+#define MTU 1500
+#define SOCK_BUFSIZE 8388608
+#define MAX_BURST_PACKETS 32
